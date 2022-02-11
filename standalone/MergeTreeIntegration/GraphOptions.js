4881eea886fe98805d636d4c2026577045cebce1
Leo <leosperling97@gmail.com>
2022-02-11 11:42:49 +0100

    add color legend
    Note that we don't use the "d3.join" stuff, because adding multiple elements in the enter and update functions is incredibly unintuitive and complicated...

--- standalone/MergeTreeIntegration/GraphOptions.js
+++ standalone/MergeTreeIntegration/GraphOptions.js
@@ -11,47 +11,51 @@ class GraphOptions {
   }
 
   render() {
-    // TopN
-    d3.select("#topN").on("input", d => {
-      const topN = d.target.value;
-      d3.select("#label_topN").text(`${topN} TopN Categories`);
-
-      this.streamGraph.streamgraph_options.topN = parseInt(topN);
-      this.streamGraph.render("streamgraph");
-      this.streamGraph.doTransform();
-
-      this.donutGraph.donut_options.topN = parseInt(topN);
-      this.donutGraph.render("donut");
-      this.donutGraph.doTransform();
-
-      // render the color legend
-      let labels = this.donutGraph.tree.getLabels();
-
-      // transform the labels to an array that is sorted by color_order
-      let labels_array = [];
-      for (let key in labels) {
-        labels_array.push([key, labels[key]])
-      }
-      labels_array = labels_array.sort((a, b) =>
-        this.donutGraph.tree.color_order.indexOf(parseInt(a[0])) - this.donutGraph.tree.color_order.indexOf(parseInt(b[0])));
-
-      d3.select("#color_legend")
-        .selectAll("div")
-        .data(labels_array)
-        .join(
-          enter => enter
-            .append("div")
-            .attr("style", d => `background-color: ${d[1]}`)
-            .text(d => this.donutGraph.labels[d[0]]),
-          update => update
-            .attr("style", d => `background-color: ${d[1]}`)
-            .text(d => this.donutGraph.labels[d[0]]),
-          exit => exit.remove(),
-        );
+    d3.select("#topN").on("input", e => {
+      const topN = e.target.value;
+      this.callback_on_input_change(topN);
     });
 
-    d3.select("#label_topN").text(`${d3.select("#topN").node().value} TopN Categories`);
+    const topN = d3.select("#topN").node().value;
+    this.callback_on_input_change(topN)
+  }
 
-    // d3.select("#color_legend").append()
+  callback_on_input_change(topN) {
+    d3.select("#label_topN").text(`${topN} TopN Categories`);
+
+    this.streamGraph.streamgraph_options.topN = parseInt(topN);
+    this.streamGraph.render("streamgraph");
+    this.streamGraph.doTransform();
+
+    this.donutGraph.donut_options.topN = parseInt(topN);
+    this.donutGraph.render("donut");
+    this.donutGraph.doTransform();
+
+    // render the color legend
+    let labels = this.donutGraph.tree.getLabels();
+
+    // transform the labels to an array that is sorted by color_order
+    let labels_array = [];
+    for (let key in labels) {
+      labels_array.push([key, labels[key]])
+    }
+    labels_array = labels_array.sort((a, b) =>
+      this.donutGraph.tree.color_order.indexOf(parseInt(a[0])) - this.donutGraph.tree.color_order.indexOf(parseInt(b[0])));
+
+    d3.selectAll("#color_legend .entry").remove();
+
+    labels_array.forEach(d => {
+      let entry = d3.select("#color_legend")
+        .append("div")
+        .attr("class", "entry")
+
+      entry.append("div")
+        .attr("style", `background-color: ${d[1]}; width: 10%; float: left;`)
+        .text("O");  // so the div is shown...
+
+      entry.append("div")
+        .attr("style", `width: 90%; float: right;`)
+        .text(this.donutGraph.labels[d[0]]);
+    });
   }
 }
