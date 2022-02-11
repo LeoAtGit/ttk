c271c86e3331cd9bbdc441b1e997dbe3463683fc
Leo <leosperling97@gmail.com>
2022-02-11 09:13:01 +0100

    add crude version of color legend

--- standalone/MergeTreeIntegration/GraphOptions.js
+++ standalone/MergeTreeIntegration/GraphOptions.js
@@ -24,11 +24,32 @@ class GraphOptions {
       this.donutGraph.render("donut");
       this.donutGraph.doTransform();
 
-      // get all of the color mapping
-      console.log(this.donutGraph.tree.getLabels());
-      console.log(this.donutGraph.tree.color_mapping);
-      console.log(this.donutGraph.labels);
+      // render the color legend
+      let labels = this.donutGraph.tree.getLabels();
+
+      // transform the labels to an array that is sorted by color_order
+      let labels_array = [];
+      for (let key in labels) {
+        labels_array.push([key, labels[key]])
+      }
+      labels_array = labels_array.sort((a, b) =>
+        this.donutGraph.tree.color_order.indexOf(parseInt(a[0])) - this.donutGraph.tree.color_order.indexOf(parseInt(b[0])));
+
+      d3.select("#color_legend")
+        .selectAll("div")
+        .data(labels_array)
+        .join(
+          enter => enter
+            .append("div")
+            .attr("style", d => `background-color: ${d[1]}`)
+            .text(d => this.donutGraph.labels[d[0]]),
+          update => update
+            .attr("style", d => `background-color: ${d[1]}`)
+            .text(d => this.donutGraph.labels[d[0]]),
+          exit => exit.remove(),
+        );
     });
+
     d3.select("#label_topN").text(`${d3.select("#topN").node().value} TopN Categories`);
 
     // d3.select("#color_legend").append()
